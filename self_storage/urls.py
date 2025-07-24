01ddea01aaf384f4ec00dd2be0d34bf94833dac4
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-07-24 21:29:27 +0500

    добавил урлы для авторизации

--- self_storage/urls.py
+++ self_storage/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('faq/', views.faq, name='faq'),
     path('my-rent/', views.my_rent, name='my_rent'),
     path('my-rent-empty/', views.my_rent, name='my_rent_empty'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
 ]
 
 if settings.DEBUG:
